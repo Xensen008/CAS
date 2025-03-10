453f2848efe10d1aaff0cad029281a31ad74a16e
xensen008 <arnabjyotikakati008@gmail.com>
2025-03-10 22:10:40 +0530

    update login error message for clarity

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -50,7 +50,7 @@ exports.login = async (req, res) => {
         // Validate password
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(400).json({ message: 'Invalid credentials' });
+            return res.status(400).json({ message: 'Invalid password' });
         }
 
         // Create token
